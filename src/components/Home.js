207da591f91dacf240dd3f89550b887d3bd96b24
Cameron <camcreel@gmail.com>
2021-08-10 11:15:21 -0400

    Add card to reviewer card

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import Movie from "./Movie";
+import ReviewerCard from "./ReviewerCard";
 
 function Home({movie}) {
     return (
         <div>
             <h1>Movie Reviews</h1>
             <Movie movie={movie}/>
+            
         </div>
+        
     )
 }
 
--- src/components/ReviewerCard.js
+++ src/components/ReviewerCard.js
@@ -1,7 +1,11 @@
-function ReviewerCard() {
+function ReviewerCard({reviewer}) {
 
+    
     return (
-        <div></div>
+        <div className="card">
+            <h5 className="card-title">{reviewer.name}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{reviewer.outlet}</h6>
+        </div>
     )
 
 }
