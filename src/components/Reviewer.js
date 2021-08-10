f45bb39c230d1c1c8abc7af951a2fb06bd643c34
Cameron <camcreel@gmail.com>
2021-08-09 20:12:54 -0400

    Create Reviewer component

---
+++ src/components/Reviewer.js
@@ -0,0 +1,11 @@
+function Reviewer({reviewer}) {
+
+    return (
+        <div>
+            <h1>{reviewer.name}</h1>
+            <h2>{reviewer.outlet}</h2>   
+        </div>
+    )
+}
+
+export default Reviewer
