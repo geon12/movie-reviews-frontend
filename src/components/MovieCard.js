aa421664153b50c046742024885b60406cdb9555
Cameron <camcreel@gmail.com>
2021-08-10 12:24:06 -0400

    add average rating to movie card

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,6 +4,7 @@ function MovieCard({movie}) {
             <div className="card">
                 <h5 className="card-title">{movie.name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{movie.year}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{movie.average_rating}</h6>
             </div>
         </div>
     )
